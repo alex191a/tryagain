10bf7d3d1e8ad93f8b45e6726d852522288b0a81
Kasper <kbla.skp@edu.mercantec.dk>
2020-06-19 11:05:47 +0200

    tilfreds?

--- App.js
+++ App.js
@@ -1,127 +1,36 @@
-import React from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
-import Constants from 'expo-constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 
-function Separator() {
-  return <View style={styles.separator} />;
-}
+import useCachedResources from './hooks/useCachedResources';
+import BottomTabNavigator from './navigation/BottomTabNavigator';
+import LinkingConfiguration from './navigation/LinkingConfiguration';
 
-export default function App() {
-  
-  var myArray = ["Bedste ornitolog EU", "Elsker fuglesang", "Har arbejdet på det lokale bibliotek i 17 år", "Har det fedeste hår", "Har pisse flotte briller", "Er noget af en frækkert" ];
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A1.38"
-          onPress={() => Alert.alert(myArray[Math.floor(Math.random(myArray) * myArray.length)])}
-        />
+const Stack = createStackNavigator();
+
+export default function App(props) {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+        <NavigationContainer linking={LinkingConfiguration}>
+          <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A1.39"
-          onPress={() => Alert.alert('Hvis du til fuglekending, selv er et fjog, så spørg Paul Hansen ornitlog')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A1.40"
-          onPress={() => Alert.alert('Det er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A1.41"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A29"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A30"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A31"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="A33"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="CO1"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-      <View>
-        <Text style={styles.title}>
-        </Text>
-        <Button
-          title="OLC"
-          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
-        />
-      </View>
-      <Separator />
-   </SafeAreaView>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight,
-    marginHorizontal: 16,
-  },
-  title: {
-    textAlign: 'center',
-    marginVertical: 8,
-  },
-  fixToText: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: '#737373',
-    borderBottomWidth: StyleSheet.hairlineWidth,
+    backgroundColor: '#fff',      
   },
 });
