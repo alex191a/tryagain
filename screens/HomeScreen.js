27fb1b707cbb828b5c5becd930eafa29715ef460
Kasper <kbla.skp@edu.mercantec.dk>
2020-06-19 11:07:16 +0200

    frotnend

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,179 +1,127 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import Constants from 'expo-constants';
 
-import { MonoText } from '../components/StyledText';
+function Separator() {
+  return <View style={styles.separator} />;
+}
 
-export default function HomeScreen() {
+export default function App() {
+  
+  var myArray = ["Bedste ornitolog EU", "Elsker fuglesang", "Har arbejdet på det lokale bibliotek i 17 år", "Har det fedeste hår", "Har pisse flotte briller", "Er noget af en frækkert" ];
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Open up the code for this screen:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change any of the text, save the file, and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-        <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-        </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A1.38"
+          onPress={() => Alert.alert(myArray[Math.floor(Math.random(myArray) * myArray.length)])}
+        />
       </View>
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A1.39"
+          onPress={() => Alert.alert('Hvis du til fuglekending, selv er et fjog, så spørg Paul Hansen ornitlog')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A1.40"
+          onPress={() => Alert.alert('Det er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A1.41"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A29"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A30"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A31"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="A33"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="CO1"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Text style={styles.title}>
+        </Text>
+        <Button
+          title="OLC"
+          onPress={() => Alert.alert('De er så ikke lavet endnu mester')}
+        />
+      </View>
+      <Separator />
+   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+    marginHorizontal: 16,
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
+  title: {
     textAlign: 'center',
+    marginVertical: 8,
   },
-  contentContainer: {
-    paddingTop: 30,
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
