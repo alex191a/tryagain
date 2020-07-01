3327aa3cdac4778c6a7b675954569ec5ec49436e
Kasper <kbla.skp@edu.mercantec.dk>
2020-07-01 11:49:40 +0200

    styles

--- button.js
+++ button.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { StyleSheet, Button, View, SafeAreaView, Text, Alert, ScrollView } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, ScrollView, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
 
 export default class RoomBtn extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+                <StatusBar hidden={true}/>
                 <Button title={this.props.name} onPress={() => this.props.func()} />
             </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+        marginHorizontal: 16,
+    },
+    title: {
+        textAlign: 'center',
+        marginVertical: 8,
+    },
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+});
